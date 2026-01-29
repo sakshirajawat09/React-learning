5e40378c74d56fe047f145f5774907e1cdfc1c46
Sakshi Rajawat <srajawat@bestpeers.com>
2026-01-29 16:25:10 +0530

    ep01 how to run react by cdn links

---
+++ App.js
@@ -0,0 +1,33 @@
+const parent = React.createElement("div", {id:"parent"},  
+  [
+    React.createElement("div", {id:"child_1"},  
+    [ React.createElement("h1", {id:"h1_tag"}, "Hii I am h1 tag" ),
+      React.createElement("h2", {id:"h2_tag"}, "Hii I am h2 tag" )
+    ]
+  ),
+  React.createElement("div", {id:"child_2"},  
+    [ React.createElement("h1", {id:"h1_tag"}, "Hii I am h1 tag" ),
+      React.createElement("h2", {id:"h2_tag"}, "Hii I am h2 tag" )
+    ]
+  )
+  ]
+)
+
+const heading = React.createElement(
+  "h1", 
+  {id: "heading"},  //attribute
+   "hello from react")
+
+   console.log(parent) //object
+const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(heading)
+root.render(parent)
+
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// function App() {
+//   return <h1>Hello Sakshi 👩‍💻</h1>;
+// }
+
+// root.render(<App />);
