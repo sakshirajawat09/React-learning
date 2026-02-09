8809f9e0fd0ff57216cea2d9fd5afb2af74301aa
Sakshi Rajawat <srajawat@bestpeers.com>
2026-02-09 17:31:36 +0530

    learn Tailwing css

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -6,14 +6,14 @@ const FALLBACK_IMG = "https://via.placeholder.com/508x320?text=No+Image";
 
 const RestaurantCard = ({ cloudinaryImageId, name, cuisines, avgRating }) => {
   return (
-    <div className="card" style={{ background: "grey" }}>
-      <img
+    <div className="card m-4 p-4 w-[250px] bg-gray-100 rounded-lg hover:bg-gray-200">
+      <img className="rounded-lg"
         src={IMG_CDN_URL + cloudinaryImageId}
         onError={(e) => {
           e.target.src = LOGO_URL;
         }}
       />
-      <h2>{name}</h2>
+      <h2 className="font-bold text-lg py-4">{name}</h2>
       <h3>{cuisines?.join(", ")}</h3>
       <h4>{avgRating} stars</h4>
     </div>
